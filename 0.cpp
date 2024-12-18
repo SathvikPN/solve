4314878daa0f77e97d9505ba6bfcca1646d608d4
SathvikPN <sathvikapn@gmail.com>
2024-12-18 13:26:00 +0530

    add to repo

---
+++ 0.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+const long long MOD = 1e9+7;
+ 
+void solution() {
+    cout << "sample code";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solution();
+    return 0;
+}
