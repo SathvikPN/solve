ec3e373022bc95c20430bf1b049ef0e04d08ad47
SathvikPN <sathvikapn@gmail.com>
2024-12-18 14:10:09 +0530

    add to repo

--- README.sh
+++ README.sh
@@ -1,3 +1,10 @@
-alias gsync="git add . && git commit -m 'add to repo' &&  git push origin main"
+#!/bin/bash
 
-# code template: start.cpp
+gsync() {
+    git add . && git commit -m 'add to repo' && git push origin main
+}
+
+nfile() {
+    cp start.cpp "$1"  # code template: start.cpp
+    code -r "$1" # open the file in current vscode window
+}
