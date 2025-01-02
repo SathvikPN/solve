7a8290d5d652736695440c62bc6415fa1e6725a2
SathvikPN <sathvikapn@gmail.com>
2025-01-02 17:00:21 +0530

    add to repo

--- subarray_divisibility.cpp
+++ subarray_divisibility.cpp
@@ -11,6 +11,33 @@ int main() {
     return 0;
 }
 
+// subarray_sum[i:j] i.e sum(i,j) = sum(0,j) - sum(0, i-1)
+// build prefix sum 
+// subarray_sum divisible by N mean, modulus is zero.
+// sum(i,j)%N = sum(0,j)%N - sum(0, i-1)%N = 0 
+// sum(0,j)%N = sum(0,i-1)%N
+// for all same modulo reminder, say k
+// all combinations kC2 contribute a subarray 
+// kC2 = (k)(k-1)/2
+
 void solution(){
-    
+    int n; cin >> n;
+    int a[n]={0};
+    long long psum=0;
+    long long pmod[n]={0};  // subarraySum % N = {0,1 .... N-1}
+    pmod[0] = 1; // learn
+
+    for(int i=0; i<n; ++i){
+        cin >> a[i];
+        psum += a[i];
+        pmod[(psum % n + n) % n] += 1; // handle negative reminder
+    }
+
+    long long subarrayCount = 0;
+    for(auto x:pmod){
+        subarrayCount = subarrayCount + x*(x-1)/2;
+    }
+
+    cout << subarrayCount;
+
 }
