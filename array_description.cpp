f0a915795573f34eb56fee40ee0c341ca630bbbc
SathvikPN <sathvikapn@gmail.com>
2025-01-04 14:04:50 +0530

    add to repo

---
+++ array_description.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+const long long MOD = 1e9+7; // 10^9 + 7
+ 
+void solution();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solution();
+    return 0;
+}
+
+void solution(){
+    int n, m; cin >> n >> m;
+    int arr[n+1]; for(int i=1; i<=n; ++i) cin >> arr[i]; // 1-based-idx
+
+    // dp[i][x]: ways to fill idx i with value x  (1<=i<=n)(1<=x<=m) 
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    // base-case: first elem of arr
+    if(arr[1] == 0) { // anything in (1...m)
+        for(int i=1; i<=m; ++i) dp[1][i] = 1;
+    } else { // only existing value contribute a way
+        dp[1][arr[1]] = 1;
+    }
+
+    for(int i=2; i<=n; ++i){
+        int v;
+        if(arr[i] == 0){
+            for(v=1; v<=m; ++v){
+                if(v-1>=1) dp[i][v] = dp[i][v] + dp[i-1][v-1]; dp[i][v]%=MOD;
+                           dp[i][v] = dp[i][v] + dp[i-1][v];   dp[i][v]%=MOD;
+                if(v+1<=m) dp[i][v] = dp[i][v] + dp[i-1][v+1]; dp[i][v]%=MOD;
+            }
+        } else {
+            v = arr[i];
+            if(v-1>=1) dp[i][v] = dp[i][v] + dp[i-1][v-1]; dp[i][v]%=MOD;
+                       dp[i][v] = dp[i][v] + dp[i-1][v];   dp[i][v]%=MOD;
+            if(v+1<=m) dp[i][v] = dp[i][v] + dp[i-1][v+1]; dp[i][v]%=MOD;
+        }
+    }
+
+    long long count = 0;
+    for(int v=1; v<=m; ++v){
+        count = count + dp[n][v];
+    }
+    cout << count % MOD;
+
+}
